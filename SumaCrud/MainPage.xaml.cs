2b73ca3d237cd4377d7d47d2e08db0f297285696
Alexis <Alexis@DESKTOP-C5RDDHT>
2024-08-19 13:19:43 -0600

    Agregar archivos de proyecto.

---
+++ SumaCrud/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿namespace SumaCrud
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly LocalDbService _dbService;
+        private int _editResultadoId;
+
+        public MainPage(LocalDbService dbService)
+        {
+            InitializeComponent();
+            _dbService = dbService;
+            Task.Run(async () => listview.ItemsSource = await _dbService.GetResultado());
+        }
+
+        private async void sumarBtn_Clicked(object sender, EventArgs e)
+        {
+            if (_editResultadoId == 0) 
+            {
+                await _dbService.Create(new Resultado
+                {
+                    Numero1 = entryPrimernumero.Text,
+                    Numero2 = entrySegundonumero.Text,
+                    Suma = labelResultado.Text
+                }); 
+            }
+            else
+            {
+                await _dbService.Update(new Resultado
+                {
+                    Id = _editResultadoId,
+                    Numero1 = entryPrimernumero.Text,
+                    Numero2 = entrySegundonumero.Text,
+                    Suma = labelResultado.Text
+                });
+                _editResultadoId = 0;
+            }
+            entryPrimernumero.Text = string.Empty;
+            entrySegundonumero.Text = string.Empty;
+            labelResultado.Text = string.Empty;
+
+            listview.ItemsSource = await _dbService.GetResultado();
+        }
+
+        private async void listview_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var resultado = (Resultado)e.Item;
+            var action = await DisplayActionSheet("Action", "Cancel", null, "Edit", "Delete");
+
+            switch (action)
+            {
+                case "Edit":
+                    _editResultadoId = resultado.Id;
+                    entryPrimernumero.Text = resultado.Numero1;
+                    entrySegundonumero.Text = resultado.Numero2;
+                    labelResultado.Text = resultado.Suma;
+                    break;
+
+                case "Delete":
+                    await _dbService.Delete(resultado);
+                    listview.ItemsSource = await _dbService.GetResultado();
+                    break;
+
+
+
+            }
+        }
+    }
+
+}
